c62d00fb3dbfecd538cfc29f7dfde670e8cfd1ee
Romain Debas <debasromain@gmail.com>
2025-12-04 19:24:38 +0100

    create members component

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,11 +1,13 @@
 import "./About.css";
 import FAQ from "../components/FAQ/FAQ";
+import Members from "../components/Members/Members";
 import Testimonials from "../components/Testimonials/Testimonials";
 
 function About() {
 	return (
 		<>
 			<h1>About</h1>
+			<Members />
 			<FAQ />
 			<Testimonials />
 		</>
