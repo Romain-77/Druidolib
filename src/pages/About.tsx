7361887dba041236853ea460b6fc8167411e41ad
Romain Debas <debasromain@gmail.com>
2025-12-04 13:21:28 +0100

    add testimonials component

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,11 +1,13 @@
 import "./About.css";
 import FAQ from "../components/FAQ/FAQ";
+import Testimonials from "../components/Testimonials/Testimonials";
 
 function About() {
 	return (
 		<>
 			<h1>About</h1>
 			<FAQ />
+			<Testimonials />
 		</>
 	);
 }
